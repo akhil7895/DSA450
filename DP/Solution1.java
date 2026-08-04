b1f8e99d1ea6937efbae00938a339d8983971316
Akhil chaturvedi <128232661+akhil7895@users.noreply.github.com>
2026-08-04 14:08:23 +0530

    coin change(count ways)

--- DP/Solution1.java
+++ DP/Solution1.java
@@ -1,5 +1,41 @@
-package DSA450.DP;
-
+import java.util.*;
 public class Solution1 {
+    public static void main(String[] args) {
+        int[] arr = {2,5,3,6};
+        int sum = 10;
+        System.out.println(count(arr,sum));
+        System.out.println(countDP(arr,sum));
+    }
+
+    //Recursive Solution
+    public static int count(int[] arr, int sum){
+        int n = arr.length;
+        return countRec(arr,n,sum);
+    }
+
+    public static int countRec(int[] arr, int i,int sum){
+        if(sum == 0 ) return 1;
+        if(sum<0 || i ==0) return 0;
+
+        return countRec(arr, i, sum - arr[i-1]) + countRec(arr, i-1, sum);
+    }
+
+    public static int countDP(int [] arr, int sum ){
+        int n = arr.length;
+        int[][] dp = new int[n][sum+1];
+        for(int [] num : dp){
+            Arrays.fill(num,-1);
+        }
+        return countDpHelper(arr,sum,dp,n);
+    }
+
+    public static int countDpHelper(int arr[] , int sum , int [][] dp,int i){
+        if(sum ==0 ) return 1;
+        if(sum <0 || i==0) return 0;
+        if(dp[i-1][sum] !=  -1) return dp[i-1][sum];
+        
+        return dp[i-1][sum] = countDpHelper(arr, sum - arr[i-1], dp, i) + 
+                            countDpHelper(arr, sum, dp, i-1);
+    }
     
 }
